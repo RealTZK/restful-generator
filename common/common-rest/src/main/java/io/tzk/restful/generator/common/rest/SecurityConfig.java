4d11978022d32405d347cb98f9d86f7df09db64f
tianzhenkun <tianzhenkun@jd.com>
2022-07-22 18:49:03 +0800

    nothing

--- common/common-rest/src/main/java/io/tzk/restful/generator/common/rest/SecurityConfig.java
+++ common/common-rest/src/main/java/io/tzk/restful/generator/common/rest/SecurityConfig.java
@@ -3,16 +3,19 @@ package io.tzk.restful.generator.common.rest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        //省略HttpSecurity的配置
-        return httpSecurity.httpBasic(withDefaults()).build();
+        return httpSecurity
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(
+                                userInfo -> userInfo.userService(new DefaultOAuth2UserService())))
+                .build();
     }
 }
--- tenant/tenant-rest/src/main/java/io/tzk/restful/generator/tenant/rest/SessionController.java
+++ tenant/tenant-rest/src/main/java/io/tzk/restful/generator/tenant/rest/SessionController.java
@@ -4,20 +4,24 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @Tag(name = "会话接口")
 public class SessionController {
 
+    @Resource
+    private OAuth2AuthorizedClientService authorizedClientService;
+
     @PostMapping("/session")
     @Operation(summary = "登录")
-    public ResponseEntity<String> login() {
-        var context = SecurityContextHolder.getContext();
-        var authentication = context.getAuthentication();
+    public ResponseEntity<String> login(OAuth2AuthenticationToken authentication) {
         return ResponseEntity.ok(authentication.getName());
     }
 
